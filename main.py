18a7e6e5790a3873ef2580a7fa189f03741bd25a
Mahmobcc <125855902+Mahmobcc@users.noreply.github.com>
2025-01-28 00:06:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import threading
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://mahmobcc.github.io/"],  # Allow all origins, but you can limit this for security
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 # Cache to store user solve counts
 cache = {}
 
@@ -18,6 +26,8 @@ def fetch_data():
         response = requests.get(f"https://uhunt.onlinejudge.org/u/{user_id}")
         if response.status_code == 200:
             cache[user_id] = {"uhunt": response.json()["solved"]}
+        else:
+            print("Not Found uHunt")
     
     # Fetch Codeforces data
     for handle in codeforces_ids:
@@ -25,6 +35,8 @@ def fetch_data():
         if response.status_code == 200:
             #solved_count = len(set([problem["contestId"] for problem in response.json()["result"] if problem["verdict"] == "OK"]))
             cache[handle] = {"codeforces": response.json()["newRating"]}
+        else:
+            print("Not Found CodeForces")
 
 # Run fetch_data every 2 hours
 def update_data():
@@ -37,4 +49,6 @@ thread.start()
 
 @app.get("/solve-counts")
 def get_solve_counts():
+    if not cache:
+        return {"message": "Cache is empty, no solve counts available."}
     return cache
